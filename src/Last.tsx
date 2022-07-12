8d542e436fccfd0df5b1975cb5f7969e234cb3d5
Kyabetsu8931Ya31 <yuuki.20020217@gmail.com>
2022-07-13 08:51:40 +0900

    update style

--- src/Last.tsx
+++ src/Last.tsx
@@ -7,7 +7,6 @@ export const Last:React.FC = () => {
     const {outorder} = useContext(OrderContext);
     return (
         <div>
-            <h1>Result!</h1>
             <div className='order'>
                 <p>{outorder.menu}</p>
                 <p>{outorder.topping}</p>
