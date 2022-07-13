68987738e9ce2253471008ff42283c95f408aeb7
yuukiiwai <yuuki.20020217@gmail.com>
2022-07-13 20:29:18 +0900

    update

--- src/providers/OutorderProvider.tsx
+++ src/providers/OutorderProvider.tsx
@@ -6,13 +6,33 @@ export interface order{
     size:string,
 }
 
+export interface step{
+    step:string,
+    value:string
+}
+
 export const OrderContext = createContext<any>({});
 
 export const OutorderProvider = (props:any)=>{
     const [outorder,setOrder] = useState<order>({menu:"",size:"",topping:""});
+    const [steps,setSteps] = useState<Array<step>>();
     return(
-        <OrderContext.Provider value={{outorder,setOrder}}>
+        <OrderContext.Provider value={{outorder,setOrder,steps,setSteps}}>
+            {steps?.map((item,key)=>{
+                <Steps
+                    step = {item.step}
+                    value = {item.value}
+                />
+            })}
             {props.children}
         </OrderContext.Provider>
     )
+}
+
+const Steps:React.FC<step> = (props:step) =>{
+    return(
+        <div>
+            {">"}{props.step}
+        </div>
+    );
 }
